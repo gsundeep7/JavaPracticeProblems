73fbd37fccbf293ee62289d0f17fae4b04a2fa06
Sundeep <86550753+gsundeep7@users.noreply.github.com>
2025-09-02 08:18:39 +0530

    Adding 3 programs(7 files) to OOPS - Inheritance

---
+++ src/test/java/test/example/Javapractice/OOPS/Inheritance/Cat.java
@@ -0,0 +1,15 @@
+package test.example.Javapractice.OOPS.Inheritance;
+
+public class Cat extends Animal{
+
+    @Override
+    void sound()
+    {
+        System.out.println("Cat meows");
+    }
+    void climb()
+    {
+        System.out.println("Cat is climbing");
+    }
+
+}
